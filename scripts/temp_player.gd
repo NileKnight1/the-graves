a21d464fa5df04e42fad66a53acb06a3ef5ae593
NileKnight0 <ammartube777@gmail.com>
2026-08-25 22:58:47 +0300

    made a temp square character and tested movemnt

---
+++ scripts/temp_player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+var SPEED = 300.0
+var JUMP_VELOCITY = -250.0
+var sprint = 0
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_pressed("sprint"):
+		SPEED = 450
+		JUMP_VELOCITY = -350.0
+		
+		print("Sprinting")
+	else:
+		SPEED = 300
+		JUMP_VELOCITY = -250.0
+		
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
