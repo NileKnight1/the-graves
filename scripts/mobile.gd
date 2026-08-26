0ea621c9f4a6fd0b0de7ac947a324afebac74fb7
NileKnight0 <ammartube777@gmail.com>
2026-08-26 07:19:50 +0300

    integrate mobile button from an old game of mine, but jump button is not working yet

---
+++ scripts/mobile.gd
@@ -0,0 +1,39 @@
+extends CanvasLayer
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
+
+#
+#
+#func _on_left_button_down() -> void: Input.action_press("left")
+#func _on_left_button_up() -> void: Input.action_release("left")
+#
+#func _on_right_button_down() -> void: Input.action_press("right")
+#func _on_right_button_up() -> void: Input.action_release("right")
+#
+#func _on_down_button_down() -> void: Input.action_press("down")
+#func _on_down_button_up() -> void: Input.action_release("down")
+#
+#func _on_up_button_down() -> void: Input.action_press("up")
+#func _on_up_button_up() -> void: Input.action_release("up")
+##
+##func _on_sprint_button_down() -> void: Input.action_press("sprint")
+##func _on_sprint_button_up() -> void: Input.action_release("sprint")
+#
+#func _on_jump_button_down() -> void: Input.action_press("jump")
+#func _on_jump_button_up() -> void: Input.action_release("jump")
+
+func _on_interact_pressed() -> void:
+	Input.action_press("interact")
+	Input.action_release("interact")
+
+
+func _on_jump_pressed() -> void:
+	print("hi")
+	Input.action_press("jump")
+	Input.action_release("jump")
